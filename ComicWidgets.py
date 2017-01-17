cb4c6a3b1fc60755dfebca129416feaf1a43b3cd
HNMN3 <hnmn3.nitin@gmail.com>
2017-01-17 15:23:35 +0530

    Color Changed

--- ComicWidgets.py
+++ ComicWidgets.py
@@ -1,33 +1,36 @@
-#Keep Coding And change the world and do not forget anything... Not Again..
+# Keep Coding And change the world and do not forget anything... Not Again..
 from kivy.uix.scatter import Scatter
 from kivy.graphics import Line
 
+
 class DraggableWidget(Scatter):
-    def __init__(self,**kwargs):
-        self.selected=None
-        self.touched=False
-        super(DraggableWidget,self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        self.selected = None
+        self.touched = False
+        super(DraggableWidget, self).__init__(**kwargs)
+
     def on_touch_down(self, touch):
-        if self.collide_point(touch.x,touch.y):
-            self.touched=True
+        if self.collide_point(touch.x, touch.y):
+            self.touched = True
             self.select()
-            super(DraggableWidget,self).on_touch_down(touch)
+            super(DraggableWidget, self).on_touch_down(touch)
             return True
         return super(DraggableWidget, self).on_touch_down(touch)
+
     def select(self):
         if not self.selected:
-            self.ix=self.center_x
+            self.ix = self.center_x
             self.iy = self.center_y
             with self.canvas:
-                self.selected = Line(rectangle=(0,0,self.width,self.height),dash_offset=2)
+                self.selected = Line(rectangle=(0, 0, self.width, self.height), dash_offset=2)
 
-    def on_pos(self,instance,value):
+    def on_pos(self, instance, value):
         if self.selected and self.touched:
             if self.parent is None or type(self.parent) != DraggableWidget:
                 return
 
             go = self.parent.general_options
-            go.translation = (self.center_x-self.ix,self.center_y-self.iy)
+            go.translation = (self.center_x - self.ix, self.center_y - self.iy)
             self.ix = self.center_x
             self.iy = self.center_y
 
@@ -45,19 +48,20 @@ class DraggableWidget(Scatter):
             go = self.parent.general_options
             go.scale = value
 
-    def translate(self,x,y):
-        self.center_x = self.ix = self.ix+x
-        self.center_y = self.iy = self.iy+y
+    def translate(self, x, y):
+        self.center_x = self.ix = self.ix + x
+        self.center_y = self.iy = self.iy + y
 
     def on_touch_up(self, touch):
         if self.selected:
             self.unselect()
             return True
         return super(DraggableWidget, self).on_touch_up(touch)
+
     def unselect(self):
         self.canvas.remove(self.selected)
-        self.selected=None
+        self.selected = None
+
 
 class StickMan(DraggableWidget):
     pass
-
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-#Keep Coding And change the world and do not forget anything... Not Again..
+# Keep Coding And change the world and do not forget anything... Not Again..
 import kivy
+
 kivy.require('1.9.1')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
-import ComicWidgets,ToolBox,GeneralOptions,DrawingSpace,StatusBar
+import ComicWidgets, ToolBox, GeneralOptions, DrawingSpace, StatusBar
 from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
 
-Window.clearcolor = (1,1,0,1)
+Window.clearcolor = get_color_from_hex("#1DE9B6")
 
 Builder.load_file('style.kv')
 Builder.load_file('toolbox.kv')
@@ -17,13 +19,15 @@ Builder.load_file('statusbar.kv')
 Builder.load_file('comicwidgets.kv')
 Builder.load_file('comiccreater.kv')
 
+
 class ComicScreenManager(ScreenManager):
     pass
+
 
 class ComicScreenManagerApp(App):
     def build(self):
         return ComicScreenManager()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     ComicScreenManagerApp().run()
